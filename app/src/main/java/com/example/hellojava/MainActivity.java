652b653365900f5fdb5d8e8b06ad29138673bbe7
bibigul_minassipovaepam.com <Bibigul_Minassipova@epam.com>
2021-12-29 16:14:32 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hellojava/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.hellojava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void setHelloJavaText(View view) {
+        TextView helloTextView = findViewById(R.id.nameTextView);
+        helloTextView.setText("Hello Java!");
+        helloTextView.setBackgroundColor(Color.YELLOW);
+        helloTextView.setTextSize(30);
+    }
+}
